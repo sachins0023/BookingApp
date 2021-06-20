33b33d95ec555f417f249a81cda21a72d9006954
sachin <sachin@aerchain.io>
2021-06-20 12:44:51 +0530

    initial commit with base page

---
+++ ticket-booking-app/pages/_app.js
@@ -0,0 +1,61 @@
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+} from "@chakra-ui/react";
+import { Global, css } from "@emotion/react";
+import customTheme from "../styles/theme";
+
+const GlobalStyle = ({ children }) => {
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Global
+        styles={css`
+          ${
+            "" /* ${colorMode === 'light' ? prismLightTheme : prismDarkTheme}; */
+          }
+          ::selection {
+            background-color: #90cdf4;
+            color: #fefefe;
+          }
+          ::-moz-selection {
+            background: #ffb7b7;
+            color: #fefefe;
+          }
+          html {
+            min-width: 356px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+            background: ${colorMode === "light" ? "white" : "#171717"};
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "dark",
+          useSystemColorMode: true,
+        }}
+      >
+        <GlobalStyle>
+          <Component {...pageProps} />
+        </GlobalStyle>
+      </ColorModeProvider>
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
